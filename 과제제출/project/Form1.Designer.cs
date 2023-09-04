87b56b6ab314f3e6410bf01f27c029c9f921479b
chung2code <anthdny7@gmail.com>
2023-09-04 10:08:06 +0900

    second subject

--- 과제제출/project/Form1.Designer.cs
+++ 과제제출/project/Form1.Designer.cs
@@ -32,10 +32,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -45,18 +55,19 @@
             "COM1",
             "COM2",
             "COM3",
-            "COM4"
-            });
-            this.comboBox1.Location = new System.Drawing.Point(60, 42);
+            "COM4"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 50);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.Size = new System.Drawing.Size(150, 26);
             this.comboBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(248, 41);
+            this.button1.Location = new System.Drawing.Point(310, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.Size = new System.Drawing.Size(151, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "연결";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,82 +76,135 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(60, 124);
+            this.button2.Location = new System.Drawing.Point(25, 28);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 95);
+            this.button2.Size = new System.Drawing.Size(151, 114);
             this.button2.TabIndex = 2;
-            this.button2.Text = "LED_01 ON";
+            this.button2.Text = "ON";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(248, 124);
+            this.button3.Location = new System.Drawing.Point(266, 28);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 95);
+            this.button3.Size = new System.Drawing.Size(151, 114);
             this.button3.TabIndex = 3;
-            this.button3.Text = "LED_01 OFF";
+            this.button3.Text = "OFF";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // groupBox1
             // 
-            this.button4.Location = new System.Drawing.Point(60, 259);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 95);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "LED_02 ON";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(26, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 180);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "LED";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // button5
+            // groupBox2
             // 
-            this.button5.Location = new System.Drawing.Point(248, 259);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(121, 95);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "LED_02 OFF";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(26, 366);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(383, 76);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "온도센서";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Location = new System.Drawing.Point(26, 519);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(383, 62);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "조도센서";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox3);
+            this.groupBox4.Location = new System.Drawing.Point(515, 519);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(358, 62);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "초음파센서";
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(60, 378);
+            this.textBox1.Location = new System.Drawing.Point(25, 27);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 18);
-            this.textBox1.TabIndex = 6;
-            this.textBox1.Text = "Message:";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Size = new System.Drawing.Size(340, 28);
+            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(60, 402);
+            this.textBox2.Location = new System.Drawing.Point(25, 27);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(309, 18);
-            this.textBox2.TabIndex = 7;
+            this.textBox2.Size = new System.Drawing.Size(340, 28);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(340, 28);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(33, 38);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(340, 333);
+            this.textBox4.TabIndex = 3;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.textBox4);
+            this.groupBox5.Location = new System.Drawing.Point(515, 28);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(418, 393);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "모니터링";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1000, 766);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -150,10 +214,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
 
--- 과제제출/project/Form1.cs
+++ 과제제출/project/Form1.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace project
 {
     public partial class Form1 : Form
+       
     {
+        private SerialPort serialport = new SerialPort();
         public Form1()
         {
             InitializeComponent();
@@ -61,6 +65,89 @@ namespace project
             this.textBox2.Text="LED_02 OFF SUCCESS";
         }
 
-       
+        private void connection_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connection Clicked..");
+            String port_Name = null;
+            try
+            {
+                if (this.comboBox1.SelectedIndex > -1)
+                {
+                    port_Name = this.comboBox1.Items[this.comboBox1.SelectedIndex].ToString();
+                    Console.WriteLine("Selected Port :" + port_Name);
+
+                    //Serial Port 연결
+                    this.serialport.PortName = port_Name;
+                    this.serialport.BaudRate = 9600;
+                    this.serialport.DataBits = 8;
+                    this.serialport.StopBits = System.IO.Ports.StopBits.One;
+                    this.serialport.Parity = System.IO.Ports.Parity.None;
+                    this.serialport.Open();
+                    Console.Write("Connection Success!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection ERROR : " + ex);
+                this.serialport.Close();
+            }
+        }
+
+        private void connection_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connection Clicked..");
+            String port_Name = null;
+            try
+            {
+                if (this.comboBox1.SelectedIndex > -1)
+                {
+                    port_Name = this.comboBox1.Items[this.comboBox1.SelectedIndex].ToString();
+                    Console.WriteLine("Selected Port :" + port_Name);
+
+                    //Serial Port 연결
+                    this.serialport.PortName = port_Name;
+                    this.serialport.BaudRate = 9600;
+                    this.serialport.DataBits = 8;
+                    this.serialport.StopBits = System.IO.Ports.StopBits.One;
+                    this.serialport.Parity = System.IO.Ports.Parity.None;
+                    this.serialport.Open();
+                    Console.Write("Connection Success!");
+                    //--------------
+                    //추가!
+                    //--------------
+                    this.serialport.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection ERROR : " + ex);
+                this.serialport.Close();
+            }
+        }
+        //--------------
+        //추가
+        //--------------
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String recvData = this.serialport.ReadExisting();
+            Console.WriteLine(recvData);
+            // UI 업데이트를 안전하게 하기 위해 Control.Invoke 사용
+            this.Invoke
+            (
+                (MethodInvoker)delegate { this.textBox1.AppendText(recvData + "\r\n"); }
+            );
+            Thread.Sleep(1000); // 10ms   //데이터 잘림막기
+            ;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
